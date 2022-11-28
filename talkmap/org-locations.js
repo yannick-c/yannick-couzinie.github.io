636e63d03514d5b66c3d3c5b63feb05a80421ffb
Yannick Couzinie <ycouzinie@quemix.com>
2022-11-28 10:26:54 +0900

    Update talkmap notebook

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,7 +1,12 @@
 var addressPoints = [
   [
-    // "Berkeley CA, USA",
-    // 37.8708393,
-    // -122.2728638
+    "Saint-\u00c9tienne-du-Rouvray, France",
+    49.3831,
+    1.09163
   ],
+  [
+    "Geneva, Switzerland",
+    46.2017559,
+    6.1466014
+  ]
 ];
